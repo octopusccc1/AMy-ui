13bd9afc0162df6696dc8a3d4e3e7f40f5ac911e
张宇 <zhangyu1@supremind.com>
2022-08-28 23:15:39 +0800

    feat: 提交

--- scripts/build.js
+++
@@ -1,6 +0,0 @@
-const webpackConfig = require('../webpack.config');
-const prodConfig = require('../webpack/webpack.prod.config')
-const { merge } = require('webpack-merge')
-module.exports = () => {
-    return merge(webpackConfig('production'), prodConfig)
-}
